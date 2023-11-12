fe8692805157357b778c09a0f0eda2e01b8d5731
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-11-12 14:40:19 +0300

    First done

--- SeminarOne/Calculator/Calculator.java
+++ SeminarOne/Calculator/Calculator.java
@@ -43,8 +43,13 @@ public class Calculator {
     // Нужно написать в калькуляторе метод вычисления суммы покупки со скидкой и проверить его, используя AssertJ
     // Примерная сигнатура и тело метода:
     public static double calculatingDiscount(double purchaseAmount, int discountAmount) {
+        if (purchaseAmount < 0 || discountAmount < 0 || discountAmount > 100){
+            throw new ArithmeticException( "Invalid Arguments.");
+        }
+        double purchaseDiscount = purchaseAmount * discountAmount / 100 ;
+
         // purchaseAmount - сумма покупки
         // discountAmount - размер скидки
-        return 0; // Метод должен возвращать сумму покупки со скидкой
+        return purchaseAmount - purchaseDiscount; // Метод должен возвращать сумму покупки со скидкой
     }
 }
